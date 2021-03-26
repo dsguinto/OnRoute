6377e7d3dda5279d6353781d04d9c763e3f08ceb
moeasakr <moeasakr@gmail.com>
2021-03-25 20:37:39 -0400

    Created database class

---
+++ models/Database.php
@@ -0,0 +1,20 @@
+<?php
+namespace OnRoute\models;
+use PDO;
+class Database{
+    private static $user ="moeasakr_the_webdevs";
+    private static $pass = "Pi&eOplg+}_N";
+    private static $dsn = "mysql:host=198.50.215.64;dbname=moeasakr_onroute";
+    private static $dbcon;
+
+    private function __construct() {
+
+    }
+
+    public static function getDB() {
+        if (!isset(self::$dbcon)) {
+            self::$dbcon = new PDO(self::$dsn, self::$user, self::$pass);
+        }
+        return self::$dbcon;
+    }
+}
