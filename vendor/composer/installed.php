22b6081a03dd36b4a69fab7206a5e9d5712190a9
naihsienchen <71660637+naihsienchen@users.noreply.github.com>
2021-03-27 15:52:04 -0400

    composer added

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '64219a4c3a2bb4d897bc3291604a7a1a4ca9a81f',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '64219a4c3a2bb4d897bc3291604a7a1a4ca9a81f',
+    ),
+  ),
+);
