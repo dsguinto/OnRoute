1c70ee99dad8263afb897b3dc84a9f0d502ea78b
Will Midgette <wamidgette@gmail.com>
2021-03-27 18:08:27 -0400

    Flight.php added, flightNumberSearch updated

--- flightNumberSearch.php
+++ flightNumberSearch.php
@@ -1,23 +1,37 @@
 <!-- Once a user has booked their flight, they can enter their flight number to see the flight details this does not require a login. It is a quicker way for customers to view their flight details, rather than logging in and checking their bookings-->
 <?php
-    use OnRoute\models\{Database,FlightsData};
+    use OnRoute\models\{Database,Flight};
+    require_once 'vendor/autoload.php';
     require_once 'library/functions.php';
+    require_once 'models/flight.php';
+    require_once 'models/database.php';
     $css = array("styles/flightTracking.css");
-    require_once 'views/header.php';
 
     var_dump($_POST);
 
-    if(isset($_POST['flightTrackSubmit'])){
+    //WHEN FORM IS SUBMITTED
+    if(isset($_POST['flightSubmit'])){
         /* header('location: flightInfo.php'); */
         //Was there a flight number submitted?
-        if($_POST['flightNumber'] == ""){
+        if($_POST['flightId'] == ""){
             echo 'Please enter a flight number';
             return false;
         }
+        $flightId = $_POST['flightId'];
+        //instantiate database connection
+        $db = Database::getDb();
+
         //When flight number submitted, instantiate db connection, utilize getFlightById
-        $FlightDataController = new FlightsData(Database::getDB());
+        $flightController = new Flight($db);
+
+        //send flightnumber to controller 
+        $response = $flightController->getFlightById($flightId);
+
+        //response from database 
+        var_dump($response);
     }
 
+    require_once 'views/header.php';
 ?>
 <main>
     <div class = "flightTrack">
@@ -26,9 +40,9 @@
         <form class ="flightTrack__form" action="" method="POST" name = "flightTrack__form">
             <div>
                 <label for="flightNumber">Flight Number</label>
-                <input type="text" id="flightNumber" name="flightNumber" placeholder="######"/>
+                <input type="text" id="flightNumber" name="flightId" placeholder="######"/>
             </div>
-            <button type="submit" name="flightTrackSubmit" value="submitted">GO!</button>
+            <button type="submit" name="flightSubmit" value="submitted">GO!</button>
         </form>
     </div>
 </main>
